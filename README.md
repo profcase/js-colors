feb1b68a27640a234b1ae345986e772fa2dfa359
Denise Case <dcase@nwmissouri.edu>
2019-07-28 21:48:15 -0500

    F19

--- README.md
+++ README.md
@@ -26,11 +26,19 @@
 
 ## See Also
 
-- [js-colors](https://github.com/profcase/js-colors)
 - [js-console](https://github.com/profcase/js-console)
+- [js-colors](https://github.com/profcase/js-colors)
 - [js-e1](https://github.com/profcase/js-e1)
 - [js-gui](https://github.com/profcase/js-gui)
+- [js-gui-basic](https://github.com/profcase/js-gui-basic)
+- [js-gui-with-tests](https://github.com/profcase/js-gui-with-tests)
+- [js-gui-pwa-fact](https://github.com/profcase/js-gui-pwa-fact)
 - [js-gui-storage](https://github.com/profcase/js-gui-storage)
 - [js-gui-vue](https://github.com/denisecase/js-gui-vue)
-- [js-node](https://github.com/denisecase/js-node)
-- [js-node-express](https://github.com/denisecase/js-node-express)
+- [node-server](https://github.com/profcase/node-server)
+- [todo-server-jwt](https://github.com/profcase/todo-server-jwt)
+- [node-express-app](https://github.com/denisecase/node-express-app)
+- [node-express-chat](https://github.com/denisecase/node-express-chat)
+- [node-express-mvc](https://github.com/denisecase/node-express-mvc)
+- [node-express-mvc-ejs-start](https://bitbucket.org/professorcase/node-express-mvc-ejs-start)
+- [node-express-mvc-ejs-passport](https://bitbucket.org/professorcase/node-express-mvc-ejs-passport)
--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+// define handler
 const updateColor = () => {
-    const r = parseInt(document.querySelector('#red').value)
-    const g = parseInt(document.querySelector('#green').value)
-    const b = parseInt(document.querySelector('#blue').value)
-    const canvasElement = document.querySelector('#canvas')
-    canvasElement.style.backgroundColor = `rgb(${r},${g},${b})`
-    canvasElement.style.border = "thin solid black"
+  const r = parseInt(document.querySelector('#red').value)
+  const g = parseInt(document.querySelector('#green').value)
+  const b = parseInt(document.querySelector('#blue').value)
+  const canvasElement = document.querySelector('#canvas')
+  canvasElement.style.backgroundColor = `rgb(${r},${g},${b})`
+  canvasElement.style.border = "thin solid black"
 }
 
+// configure event listeners
 document.querySelector('#red').addEventListener('click', updateColor)
 document.querySelector('#blue').addEventListener('click', updateColor)
 document.querySelector('#green').addEventListener('click', updateColor)
+document.querySelector('#red').addEventListener('touchend', updateColor)
+document.querySelector('#blue').addEventListener('touchend', updateColor)
+document.querySelector('#green').addEventListener('touchend', updateColor)
 
-updateColor()
+// call the handler 
+updateColor() 
